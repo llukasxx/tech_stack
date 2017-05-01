33c4e2e7604e409df2dbef9490ee49b78c4e3c9e
Łukasz Gorzelany <llukasxx@gmail.com>
2017-05-01 11:17:33 +0200

    Add initial reducer

---
+++ src/components/LibraryList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    libraries: state.libraries
+  };
+};
+
+export default connect(mapStateToProps)(LibraryList);
