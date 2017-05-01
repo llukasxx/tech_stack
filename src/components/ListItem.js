f2732d3e602bc00403b3673a4782d0668dd092f5
Łukasz Gorzelany <llukasxx@gmail.com>
2017-05-01 14:54:18 +0200

    Add more reducers

---
+++ src/components/ListItem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  static propTypes = {
+    library: PropTypes.object.isRequired
+  }
+  render() {
+    const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+export default connect(null, actions)(ListItem);
